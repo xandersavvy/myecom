726a9441d24dcac30806e9360d5ced3fe699dbb3
souvik ghosh <xandersavy@gmail.com>
2022-01-19 11:33:29 +0530

    error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,13 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Something went wrong';
 
+
+    //wrong mongodb id error
+    if (err.name === 'CastError') {
+        err = new ErrorHandler(400, 'Product not found');
+    }
+
+    
     res.status(err.statusCode).json({
         status: err.status,
         success: false,
